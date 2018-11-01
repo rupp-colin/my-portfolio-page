7d186292261d7261898dd04deee426c1c9a91b32
Colin Rupp <colin@thinkful.com>
2018-11-01 16:06:41 -0700

    fixed some styling issues

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './header.css';
 
 export default function Header(props){
   return (
-    <div>
-      <h1 id="i-am-colin">Colin Rupp</h1>
-      <p>junior full stack developer</p>
+    <div className="row header">
+      <div className="col-12">
+        <h1 id="i-am-colin">Colin Rupp</h1>
+      </div>
+      {/*<p className="col-12">full stack developer</p>*/}
     </div>
   )
 }
--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -4,10 +4,10 @@ import './main-nav.css';
 export default function MainNav(props) {
   return (
     <div className="row main-nav">
-      <p className="col-3">Home</p>
-      <p className="col-3">About Me</p>
-      <p className="col-3">Projects</p>
-      <p className="col-3">Contact</p>
+      <p className="nav-link col-3">Home</p>
+      <p className="nav-link col-3">About Me</p>
+      <p className="nav-link col-3">Projects</p>
+      <p className="nav-link col-3">Contact</p>
     </div>
   )
 }
