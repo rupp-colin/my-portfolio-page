7069add0bb57134bbc0e57db5ed2493d0409a713
Colin Rupp <colin@thinkful.com>
2018-11-02 16:52:21 -0700

    added contact link icons and stylings

--- src/components/contact-links.js
+++ src/components/contact-links.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import GitIcon from '../github_circle_black.png';
+import EmailIcon from '../email_circle_black.png';
+import LinkedInIcon from '../linkedin_circle_black-512.png';
+import './contact-links.css';
 
 export default function ContactLinks(props) {
   return (
-    <div>
-      <ul>
-        <li>Email</li>
-        <li>Resume</li>
-        <li>GitHub</li>
-        <li>LinkedIn</li>
-      </ul>
+    <div className="row contact-links">
+      <div className="col-4 contact-icon">
+        <img src={LinkedInIcon}></img>
+      </div>
+      <div className="col-4 contact-icon">
+        <img src={GitIcon}></img>
+      </div>
+      <div className="col-4 contact-icon">
+        <img src={EmailIcon}></img>
+      </div>
+
     </div>
   )
 }
--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -4,10 +4,9 @@ import './main-nav.css';
 export default function MainNav(props) {
   return (
     <div className="row main-nav">
-      <p className="nav-link col-3">Home</p>
-      <p className="nav-link col-3">About Me</p>
-      <p className="nav-link col-3">Projects</p>
-      <p className="nav-link col-3">Contact</p>
+      <p className="nav-link col-4">About Me</p>
+      <p className="nav-link col-4">Projects</p>
+      <p className="nav-link col-4">Contact</p>
     </div>
   )
 }
