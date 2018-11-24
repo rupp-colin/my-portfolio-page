e828e61e9d18c102d44c710d41c4bc160b673808
Colin Rupp <colin@thinkful.com>
2018-11-24 15:40:08 -0800

    adds flex-week project

---
+++ src/components/project-spacegame.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import TitleScreenShot from '../TitleScreenShot.png';
+import ScreenShot from '../screenshot.png';
+
+export default class SpaceGame extends React.Component {
+
+  state = {
+    moreInfo: true
+  }
+
+  showHide() {
+    this.setState({
+      moreInfo: !this.state.moreInfo
+    })
+  }
+
+  render() {
+    const moreInfo = this.state.moreInfo;
+    return (
+      <div className="row project-right">
+        <div className="col-6 pic-container">
+          <a
+            href=""
+            target="_blank"
+            ref="noopener noreferrer"
+          >
+            <img src={TitleScreenShot} alt="title screen for game"></img>
+          </a>
+          <a
+            href=""
+            target="_blank"
+            ref="noopener noreferrer"
+          >
+            <img src={ScreenShot} alt="screen shot of Definitely Not Galaga"></img>
+          </a>
+        </div>
+        <div className="col-6">
+          <h2>Definitely Not Galaga</h2>
+          <p>"Definitely Not Galaga" is just what it sounds like.  It is
+            NOT the retro arcade game Galaga.  It IS, however,
+            a proof of concept game that resembles the old arcade
+            space shooters that I grew up playing.  This project started as
+            a challenge to myself to build something in a week using a
+            language I had previously not learned. Four days later, "Definitely
+            Not Galaga" was born after studying a bit of python and familiarizing
+            myself with the python arcade library.
+          </p>
+          {!moreInfo && <p className="more" onClick={() => this.showHide()}>more >>></p>}
+          {moreInfo === true && <TechList hide={() => this.showHide()}/>}
+        </div>
+      </div>
+    )
+  }
+  }
+
+  function TechList(props) {
+    return (
+      <div>
+        <ul className="col-12">
+          <li className="tech-icon">
+            <i className="devicon-python-plain"></i>
+          </li>
+        </ul>
+        <p className="less" onClick={() => props.hide()}>less {"<<<"}</p>
+      </div>
+    )
+  }
--- src/components/projects-board.js
+++ src/components/projects-board.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './projects-board.css';
 import Eatinarary from './project-eatinarary.js';
+import SpaceGame from './project-spacegame.js';
 
 export default function ProjectBoard(props) {
   return (
@@ -8,7 +9,7 @@ export default function ProjectBoard(props) {
       <h2 className="row" id="project-board-header">Projects</h2>
         <Eatinarary />
         <br/>
-      {Project()}
+        <SpaceGame />
     </div>
   )
 }
