195fb9d205b5a8a3492dc0bed51035db77348f1b
Colin Rupp <colin@thinkful.com>
2019-01-03 10:24:51 -0800

    adds contact icons to footer and fixes some styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactLinks from './contact-links.js';
 import './footer.css';
 
 export default function Footer(props) {
@@ -7,7 +8,7 @@ export default function Footer(props) {
       <h2 className="row">Lets talk!</h2>
       <div className="row links-container">
         <p className="col-12 contact-link">
-          <a href="https://docs.google.com/document/d/1VaGXz6-6S9AhlMHQcSEarB54jRGqkumdGURiMAuH93M/edit?usp=sharing">Colin Rupp</a>
+          <a href="https://docs.google.com/document/d/1VaGXz6-6S9AhlMHQcSEarB54jRGqkumdGURiMAuH93M/edit?usp=sharing">resume</a>
         </p>
         <p className="col-12 contact-link">
           <a href="mailto:rupp.colin@gmail.com">rupp.colin@gmail.com</a>
@@ -19,6 +20,7 @@ export default function Footer(props) {
           <a href="https://www.linkedin.com/in/colin-r-5b790b167/">LinkedIn</a>
         </p>
       </div>
+      <ContactLinks />
       <div className="spacer">
       </div>
 
